929e404d5f9bd52ba38373a9127518d7811e33eb
le-nate <99023298+le-nate@users.noreply.github.com>
2024-05-08 11:34:11 +0200

    create: script to process survey responses

---
+++ scripts/process_survey.py
@@ -0,0 +1,13 @@
+"""Process survey responses of perceived and expected inflation"""
+
+import logging
+
+from preprocess import final_df_dict
+
+
+def main() -> None:
+    """Run script"""
+
+
+if __name__ == "__main__":
+    main()
