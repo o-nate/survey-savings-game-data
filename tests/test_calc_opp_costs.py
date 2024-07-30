72023b872378d8416b0aedd95159d11b263fb3a7
le-nate <99023298+o-nate@users.noreply.github.com>
2024-07-30 15:24:15 +0200

    add: test to confirm cost and savings adds to max

--- tests/test_calc_opp_costs.py
+++ tests/test_calc_opp_costs.py
@@ -12,15 +12,43 @@ set_external_module_log_levels("warning")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
+TEST_PARTICIPANT_CODE = "17c9d4zc"
+
 logger.info("Initiating tests")
 
-logger.info("Testing calculation")
+logger.info("Testing late cost makes sense at month 1")
 assert (
-    df_opp_cost[
-        (df_opp_cost["participant.label"] == "xVRXlxl")
-        & (df_opp_cost["participant.round"] == 2)
-    ]["late"].iat[0]
+    df_opp_cost[df_opp_cost["participant.code"] == TEST_PARTICIPANT_CODE]["late"].iat[0]
     == 0
 )
+
+logger.info("Testing costs and savings add to maximum")
+total_costs = (
+    df_opp_cost[
+        (df_opp_cost["participant.code"] == TEST_PARTICIPANT_CODE)
+        & (df_opp_cost["month"] == 120)
+    ][["early", "late", "excess"]]
+    .sum(axis=1)
+    .values[0]
+)
+savings = df_opp_cost[
+    (df_opp_cost["participant.code"] == TEST_PARTICIPANT_CODE)
+    & (df_opp_cost["month"] == 120)
+]["sreal"].iat[0]
+max_savings = df_opp_cost[
+    (df_opp_cost["participant.code"] == TEST_PARTICIPANT_CODE)
+    & (df_opp_cost["month"] == 120)
+]["soptimal"].iat[0]
+
+logger.debug(
+    "total costs (%s) + savings (%s) %s = max savings (%s)",
+    total_costs,
+    savings,
+    total_costs + savings,
+    max_savings,
+)
+
+assert total_costs + savings == max_savings
+
 
 logger.info("Test complete")
