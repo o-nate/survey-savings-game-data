ced480f4afb336badedd85698a2ba33812defe88
le-nate <99023298+o-nate@users.noreply.github.com>
2024-07-30 11:24:43 +0200

    create: constants module

---
+++ scripts/utils/constants.py
@@ -0,0 +1,7 @@
+"""Constants for scripts modules"""
+
+# * Define `decision quantity` measure
+DECISION_QUANTITY = "cum_decision"
+
+# * Define purchase window, i.e. how many months before and after inflation phase change to count
+WINDOW = 3
