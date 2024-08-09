7f73c5f282f9655bf0d261bb9cdaac4e182111cd
le-nate <99023298+o-nate@users.noreply.github.com>
2024-08-09 21:00:31 +0200

    create: knowledge measures module

---
+++ src/knowledge.py
@@ -0,0 +1,68 @@
+"""Measures of financial literacy, numeracy, and abilities to calculate compound interest"""
+
+import logging
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+from src import calc_opp_costs, discontinuity, process_survey
+
+from src.preprocess import final_df_dict
+from src.utils import constants, helpers
+from src.utils.logging_helpers import set_external_module_log_levels
+
+# * Logging settings
+logger = logging.getLogger(__name__)
+set_external_module_log_levels("error")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def count_correct_responses(data: pd.DataFrame, knowledge_measure: str) -> pd.Series:
+
+    if knowledge_measure == "financial_literacy":
+        _criteria = [
+            data["Finance.1.player.finK_1"].eq(1)
+            & data["Finance.1.player.finK_2"].eq(-1)
+            & data["Finance.1.player.finK_9"].eq(1)
+        ]
+    if knowledge_measure == "numeracy":
+        _criteria = [
+            data["Numeracy.1.player.num_2b"].eq(20)
+            | data["Numeracy.1.player.num_3"].eq(50)
+        ]
+    if knowledge_measure == "compound":
+        _criteria = [
+            data["Inflation.1.player.infCI_1"].eq(1100)
+            & data["Inflation.1.player.infCI_2"].eq(2)
+            & data["Inflation.1.player.infCI_3"].eq(2)
+            & data["Inflation.1.player.infCI_4"].eq(32000)
+        ]
+    choices = [1]
+    return np.select(_criteria, choices, default=0)
+
+
+def create_knowledge_dataframe() -> pd.DataFrame:
+    dataframes = []
+    for i, j in zip(
+        ["Finance", "Numeracy", "Inflation"],
+        ["financial_literacy", "numeracy", "compound"],
+    ):
+        _df = final_df_dict[i].copy()
+        _df[j] = count_correct_responses(_df, j)
+        dataframes.append(_df[["participant.label", j]])
+    return helpers.combine_series(dataframes, how="left", on="participant.label")
+
+
+def main() -> None:
+    """Run script"""
+    df = create_knowledge_dataframe()
+    print(df.head())
+    logger.debug(df[df["participant.label"] == "9xHTKNJ"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_knowledge.py
@@ -0,0 +1,80 @@
+"""Test knowledge measures module"""
+
+import logging
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+from src import calc_opp_costs, discontinuity, process_survey
+
+from src.knowledge import count_correct_responses, create_knowledge_dataframe
+from src.preprocess import final_df_dict
+from src.utils.logging_helpers import set_external_module_log_levels
+
+from tests.utils import constants
+
+# * Logging settings
+logger = logging.getLogger(__name__)
+set_external_module_log_levels("error")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+logger.info("Testing knowledge measures")
+
+df = final_df_dict["Finance"].copy()
+measure = "financial_literacy"
+df[measure] = count_correct_responses(df, measure)
+result = df[df["participant.code"] == constants.FIN_LIT_PARTICIPANT_CODE][
+    measure
+].values[0]
+assert result == constants.SCORE
+
+
+df = final_df_dict["Numeracy"].copy()
+measure = "numeracy"
+df[measure] = count_correct_responses(df, measure)
+result = df[df["participant.code"] == constants.NUMERACY_PARTICIPANT_CODE_2B][
+    measure
+].values[0]
+assert result == constants.SCORE
+
+result = df[df["participant.code"] == constants.NUMERACY_PARTICIPANT_CODE_3][
+    measure
+].values[0]
+assert result == constants.SCORE
+
+result = df[df["participant.code"] == constants.NUMERACY_PARTICIPANT_CODE_3_NOT][
+    measure
+].values[0]
+assert result != constants.SCORE
+
+
+df = final_df_dict["Inflation"].copy()
+measure = "compound"
+df[measure] = count_correct_responses(df, measure)
+result = df[df["participant.code"] == constants.COMPOUND_PARTICIPANT_CODE][
+    measure
+].values[0]
+assert result == constants.SCORE
+
+logger.info("Testing knowledge dataframe creation")
+df = create_knowledge_dataframe()
+result_fin = df[
+    df["participant.label"] == constants.KNOWLEDGE_DATAFRAME_PARTICIPANT_LABEL
+]["financial_literacy"].values[0]
+assert result_fin == constants.KNOWLEDGE_DATAFRAME_FIN_LIT_SCORE
+df = create_knowledge_dataframe()
+result_fin = df[
+    df["participant.label"] == constants.KNOWLEDGE_DATAFRAME_PARTICIPANT_LABEL
+]["numeracy"].values[0]
+assert result_fin == constants.KNOWLEDGE_DATAFRAME_NUM_SCORE
+df = create_knowledge_dataframe()
+result_fin = df[
+    df["participant.label"] == constants.KNOWLEDGE_DATAFRAME_PARTICIPANT_LABEL
+]["compound"].values[0]
+assert result_fin == constants.KNOWLEDGE_DATAFRAME_COMPOUND_SCORE
+
+logger.info("Testing complete")
