9a844774a8c90468fdddeb9e49fd05c828f58e19
le-nate <99023298+o-nate@users.noreply.github.com>
2024-11-08 13:00:36 +0100

    add: export plot function

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,10 +1,12 @@
 """Helper functions"""
 
 import logging
+from pathlib import Path
 import sys
 from typing import List
 
 from functools import reduce
+import matplotlib.pyplot as plt
 import pandas as pd
 from src.utils.logging_helpers import set_external_module_log_levels
 
@@ -29,3 +31,18 @@ def combine_series(dataframes: List[pd.DataFrame], **kwargs) -> pd.DataFrame:
         pd.DataFrame: Combined dataframe
     """
     return reduce(lambda left, right: pd.merge(left, right, **kwargs), dataframes)
+
+
+def export_plot(
+    exported_file_path: Path, file_name: str, export_all_plots: bool
+) -> None:
+    """Export plot to results folder in png format
+
+    Args:
+        file_name (str): name of plot image file
+    """
+    file_path = exported_file_path / file_name
+    if export_all_plots:
+        plt.savefig(file_path, bbox_inches="tight")
+    elif input(f"Export {file_name}? (y) ").lower() == "y":
+        plt.savefig(file_path, bbox_inches="tight")
