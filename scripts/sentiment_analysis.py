7c5273cfecbe56dfedd2991c4e7962dc14835bc3
le-nate <99023298+o-nate@users.noreply.github.com>
2025-02-14 12:30:26 +0100

    add: sentiment analyzer now working!

--- scripts/sentiment_analysis.py
+++ scripts/sentiment_analysis.py
@@ -1,10 +1,9 @@
 """Sentiment analysis of post-experiment survey responses"""
 
 # %%
-import logging
-from pathlib import Path
-import sys
+from typing import Type
 
+import duckdb
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -12,7 +11,8 @@ from pingouin import mediation_analysis
 import seaborn as sns
 import statsmodels.formula.api as smf
 from statsmodels.iolib.summary2 import summary_col
-from transformers import pipeline
+from tqdm.auto import tqdm
+from transformers import pipeline, Pipeline
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
@@ -27,18 +27,12 @@ from src import (
     knowledge,
     process_survey,
 )
-
-from src.preprocess import final_df_dict
-from src.stats_analysis import (
-    create_bonferroni_correlation_table,
-    create_pearson_correlation_matrix,
-    run_forward_selection,
-    run_treatment_forward_selection,
-)
-from src.utils.helpers import combine_series, export_plot
+from src.utils.database import create_duckdb_database, table_exists
 from src.utils.logging_config import get_logger
 
 logger = get_logger(__name__)
+logger.debug(constants.DATABASE_FILE)
+con = duckdb.connect(constants.DATABASE_FILE, read_only=False)
 
 # * Pandas settings
 pd.options.display.max_columns = None
@@ -47,10 +41,56 @@ pd.options.display.max_rows = None
 # * Decimal rounding
 pd.set_option("display.float_format", lambda x: "%.2f" % x)
 
-MODEL = f"cardiffnlp/twitter-xlm-roberta-base-sentiment-multilingual"
+MODEL = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
+
+if table_exists(con, "sessionResults") == False:
+    create_duckdb_database(con, initial_creation=True)
+df_responses = con.sql("SELECT * FROM sessionResults").df()
+
+for q in ["q2", "q3"]:
+    print(len(df_responses[df_responses[f"sessionResults.1.player.{q}"].isna()]))
+
+# %%
+# Clean data
+for q in ["q2", "q3"]:
+    df_responses[f"sessionResults.1.player.{q}"].fillna("", inplace=True)
+    print(len(df_responses[df_responses[f"sessionResults.1.player.{q}"].isna()]))
 
 # %%
 # Use a pipeline as a high-level helper
-pipe = pipeline("text-classification", model=MODEL, from_tf=True)
+sentiment_task = pipeline("sentiment-analysis", model=MODEL, tokenizer=MODEL)
 
 # %%
+example = df_responses["sessionResults.1.player.q3"].iat[0]
+print(example)
+
+# %%
+sentiment_task(example)
+
+
+# # %%
+# def analyze_sentiment(
+#     text: str, sentiment_analyzer: Type[Pipeline]
+# ) -> list[dict[str, str | float]]:
+#     return sentiment_analyzer(text)
+
+
+# %%
+# Analyze sentiments of both open-ended survey questions
+tqdm.pandas()
+for q in ["q2", "q3"]:
+    df_responses[f"sentiment_{q}"] = df_responses[
+        f"sessionResults.1.player.{q}"
+    ].progress_apply(lambda text: sentiment_task(text))
+    s = df_responses.pop(f"sentiment_{q}").explode()
+    df_responses = df_responses.join(
+        pd.DataFrame(s.tolist(), index=s.index).rename(
+            columns={"label": f"label_{q}", "score": f"score_{q}"}
+        )
+    )
+    df_responses.value_counts(f"label_{q}")
+
+# %%
+df_responses[
+    [c for c in df_responses.columns if any(q in c for q in ["q2", "q3"])]
+].head()
